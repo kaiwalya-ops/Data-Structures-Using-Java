3fc54b8db62b42cb8fc0c551669d781414cbc12c
thedeshpande <67866250+thedeshpande@users.noreply.github.com>
2023-09-06 11:19:07 +0000

    Section Where ArrayList were implemented

--- Main.java
+++ Main.java
@@ -1,18 +1,43 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 public class Main {
   public static void main(String[] args) {
-		SingleDimensionArray sda = new SingleDimensionArray(4);
-  	TwoDimensionalArray tda = new TwoDimensionalArray(2,2);
-		sda.insert(0,0);
-		sda.insert(1,1);
-		sda.insert(2,2);
-		sda.insert(3,3);
-		sda.traverseArray();
+		ArrayList<String> stringList = new ArrayList<>();
+		stringList.add("A");
+		stringList.add("B");
+		stringList.add("C");
+		stringList.add("D");
+		stringList.add("E");
+		for(int i=0;i<stringList.size();i++){
+			System.out.print(stringList.get(i));
+		}
+		System.out.println();
+		for(String letter:stringList){
+			System.out.print(letter);
+		}
+		System.out.println();
+		Iterator<String> iterator = stringList.iterator();
+		while(iterator.hasNext()){
+			System.out.print(iterator.next());
+		}
+		System.out.println();
 
-		tda.insertValueInTheArray(0,0,0);
-		tda.insertValueInTheArray(0,1,1);
-		tda.insertValueInTheArray(1,0,2);
-		tda.insertValueInTheArray(1,1,3);
-		tda.traverse2DArray();
+		for(String letter: stringList){
+			if(letter.equals("D")){
+				System.out.println("The Element is Found!!");
+				break;
+			}
+		}
+
+		int index = stringList.indexOf("C");
+		System.out.println("The Element : C is at Index : "+index);
+		index = stringList.indexOf("F");
+		System.out.println("The Element : F is at Index : "+index);
+
+		stringList.remove(2);
+		System.out.println(stringList);
+
+		
   }
 }
