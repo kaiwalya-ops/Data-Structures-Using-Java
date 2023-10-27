0524867ab712a4d704968bbb8c31c23db55fd21a
Kaiwalya Deshpande <kaiwalya@live.in>
2023-10-28 02:20:19 +0530

    Fibonacci Using Recursion

---
+++ Fibonacci.java
@@ -0,0 +1,7 @@
+public class Fibonacci {
+    public static int fibonacci(int n){
+        if (n<0) return -1;
+        if(n == 0 || n ==1 ) return n;
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
--- Main.java
+++ Main.java
@@ -1,15 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    QueueList queue = new QueueList();
-    System.out.println(queue.isEmpty());
-    queue.enQueue(10);
-    queue.enQueue(20);
-    queue.enQueue(30);
-    System.out.println(queue.deQueue());
-    System.out.println(queue.peek());
-    System.out.println(queue.deQueue());
-    System.out.println(queue.deQueue());
-    System.out.println(queue.peek());
-    queue.deleteQueue();
+    System.out.println(Fibonacci.fibonacci(3));
   }
 }
